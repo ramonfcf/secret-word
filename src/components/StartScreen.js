2cb847e6b431021e66faca9c945594f2e52024be
ramonfcf <ramonfcf@gmail.com>
2022-07-12 09:07:57 -0300

    💄 ✨ Creating first page with style

---
+++ src/components/StartScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./StartScreen.css"
+
+const StartScreen = () => {
+  return (
+    <div className='start'>
+      <h1>Secret Words</h1>
+      <p>Clique no botão abaixo para começar</p>
+      <button>Começar</button>
+    </div>
+  )
+}
+
+export default StartScreen
