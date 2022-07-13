aeda3247f6188bfba7c119754089a09e9bb99e05
ramonfcf <ramonfcf@gmail.com>
2022-07-13 17:38:30 -0300

    🚧 Creating inicial components with their css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import { useCallback, useEffect, useState } from 'react';
+import { wordsList } from './data/words';
 import StartScreen from './components/StartScreen';
+import Game from './components/Game';
+import GameOver from './components/GameOver';
+
+const stages = [
+  { id: 1, name: 'Start' },
+  { id: 2, name: 'Game' },
+  { id: 3, name: 'End' }
+];
 
 function App() {
+  const [gameStage, setGameStage] = useState(stages[0].name);
+  const [words] = useState(wordsList);
+
+  console.log(words);
+
   return (
     <div className="App">
-      <StartScreen />
+      {gameStage === 'Start' && <StartScreen />}
+      {gameStage === 'Game' && <Game />}
+      {gameStage === 'End' && <GameOver />}
     </div>
   );
 }
